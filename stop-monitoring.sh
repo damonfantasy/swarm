9dc044184183ed8b95a0ce0784c2841023d66cc0
damonfantasy <damonfantasy@hotmail.com>
2025-07-11 17:13:18 +0200

    Update stop-monitoring.sh
    Ajout des echo

--- stop-monitoring.sh
+++ stop-monitoring.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Arrêt du monitoring"
+
 # Arrêt de Grafana
 docker stack rm grafana
 
@@ -13,4 +15,4 @@ sleep 15
 #Arrêt des 2 capteurs
 docker stack rm monitoring
 
-#Le stack MY-APP peut maintenant être arrêté
+echo "Le stack MY-APP peut maintenant être arrêté"
