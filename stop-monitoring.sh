7355ee42cb277ca91dbed6c2564082bee7029c5a
Laurent <laurent@hotmail.com>
2025-07-11 16:40:11 +0200

    Ajout des commentaires

--- stop-monitoring.sh
+++ stop-monitoring.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Arrêt de Grafana
 docker stack rm grafana
 
 sleep 15
 
+#Arrêt de Prometheus
 docker stack rm prometheus
 
 sleep 15
 
-docker stack rm monitoring 
+#Arrêt des 2 capteurs
+docker stack rm monitoring
+
+#Le stack MY-APP peut maintenant être arrêté
